6bd012ea758766e125f6487596a8ee7bcc6b87c5
Juan David gonzalez <juandavidgonzalez@gmail.com>
2026-01-12 14:59:28 -0500

    Se completó la lista de productos con ngFor, ngIf y pipes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule, DatePipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule],
+  imports: [
+    FormsModule,
+    CommonModule,
+    DatePipe,
+    UpperCasePipe
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+
+  titulo = 'Lista de productos';
+  fecha = new Date();
+
+  mostrar = true;
 
   nuevoProducto = '';
   productos: string[] = ['Zapatos', 'Camisa', 'Pantalón'];
@@ -18,6 +29,10 @@ export class App {
       this.productos.push(this.nuevoProducto);
       this.nuevoProducto = '';
     }
+  }
+
+  cambiar() {
+    this.mostrar = !this.mostrar;
   }
 
 }
